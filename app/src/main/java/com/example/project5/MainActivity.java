36f4bdc0d48dc24b87ca4dc70de9c6e84c276cb7
Gent B <gentblaku@gmail.com>
2021-04-21 23:52:29 -0400

    Testing Intents

--- app/src/main/java/com/example/project5/MainActivity.java
+++ app/src/main/java/com/example/project5/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.project5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageButton myButton = (ImageButton) findViewById(R.id.mmDonutImage);
+        myButton.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick (View v) {
+                Intent intent = new Intent(MainActivity.this, OrderingDonuts.class);
+                startActivity(intent);
+            }
+
+
+        });
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
 }
