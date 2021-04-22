3810e7d845014929e65a1e5330c0d8486cceaa4a
Fenguinn <salem.abdulla15@gmail.com>
2021-04-22 02:25:31 -0400

    Fixing master 69 420
    I da best

--- app/src/main/java/com/example/project5/MainActivity.java
+++ app/src/main/java/com/example/project5/MainActivity.java
@@ -2,28 +2,31 @@ package com.example.project5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton donutImageButton;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton button = (ImageButton) findViewById(R.id.mmDonutImage);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, OrderingDonuts.class);
-                startActivity(i);
-            }
-        });
+        donutImageButton = (ImageButton) findViewById(R.id.mmDonutImage);
+        donutImageButton.setOnClickListener(this);
 
 
     }
-
+    public void onClick(View view) {
+        Intent i = new Intent(MainActivity.this, OrderingDonuts.class);
+        startActivity(i);
+    }
 
 }
--- app/src/main/java/com/example/project5/OrderingDonuts.java
+++ app/src/main/java/com/example/project5/OrderingDonuts.java
@@ -3,7 +3,6 @@ package com.example.project5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.AdapterView;
 
 public class OrderingDonuts extends AppCompatActivity {
 
@@ -12,15 +11,4 @@ public class OrderingDonuts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordering_donuts);
     }
-
-//
-//    public void onItemSelected(AdapterView<?> parent, View view,
-//                               int pos, long id) {
-//        // An item was selected. You can retrieve the selected item using
-//        // parent.getItemAtPosition(pos)
-//    }
-//
-//    public void onNothingSelected(AdapterView<?> parent) {
-//        // Another interface callback
-//    }
 }
