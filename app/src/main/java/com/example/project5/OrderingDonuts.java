93f098ecd4f12765d738792b64c8d4b8e9d2ee7b
Gent B <gentblaku@gmail.com>
2021-04-21 21:07:42 -0400

    Minor progress on Donuts

--- app/src/main/java/com/example/project5/OrderingDonuts.java
+++ app/src/main/java/com/example/project5/OrderingDonuts.java
@@ -3,6 +3,7 @@ package com.example.project5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
 
 public class OrderingDonuts extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class OrderingDonuts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordering_donuts);
+    }
+
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
